fe3febe8f7b96fd72841770873d77394f9d98e43
MATEOCM <mateocifuentesmarin@gmail.com>
2023-09-26 10:20:16 -0500

    Se agregaron nuevos componentes al archivo post, para reducir su tamaño

---
+++ resources/views/components/div-count.blade.php
@@ -0,0 +1,6 @@
+<div class="px-2">
+    <span class="text-gray-700 text-sm">
+        <span class="flex sm:inline font-bold items-center justify-center">{{$num}}</span>
+        <span class="flex sm:inline font-normal items-center justify-center">{{$text}}</span>
+    </span>
+</div>
